3fdb81f0f547965674d3630e80f5ce902033769e
SirAnthony <anthony@adsorbtion.org>
2020-02-23 22:48:04 +0500

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup
+from os import path
+cur_dir = path.abspath(path.dirname(__file__))
+with open(path.join(cur_dir, 'README.markdown'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='SLPP',
     description='SLPP is a simple lua-python data structures parser',
+    long_description=long_description
+    long_description_content_type='text/markdown'
     version='1.1',
     author='SirAnthony',
     url='https://github.com/SirAnthony/slpp',
